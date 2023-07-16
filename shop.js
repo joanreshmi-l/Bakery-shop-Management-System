def50388d3ae4e41496a45caedb4b016decfa2e3
JOANRESHMI <111834540+joanreshmi-l@users.noreply.github.com>
2023-07-16 15:35:23 +0530

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,117 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".cake-row", {
+    spaceBetween: 30,
+    
+    centerSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
+
+
+
+
+
+ var swiper = new Swiper(".about-row", {
+    spaceBetween: 30,
+    
+    centerSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+  }); 
+
+
+
+  var swiper = new Swiper(".review-row", {
+    spaceBetween: 30,
+    
+    centerSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        
+      },
+      1024: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });
+
+  
+
+
+
+  
